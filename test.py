28bbc028c251fdd0d17b79603c0f5338f73f33d9
neel <diptoneelde@gmail.com>
2025-10-18 10:21:01 +0200

    Fix testing script

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import sys
 import joblib
 import pandas as pd
 
-data = pd.DataFrame([(sys.argv[1], sys.argv[2])], columns=["seconds_of_day", "ambience_lux"])
+data = pd.DataFrame([(sys.argv[2], sys.argv[1])], columns=["ambience_lux", "seconds_of_day"])
 
 try:
-    luminosity_model = joblib.load('street_light_model.pkl')
+    luminosity_model = joblib.load('model/street_light_model.pkl')
 except FileNotFoundError:
     print("Error: Model file 'street_light_model.pkl' not found.")
     exit()
